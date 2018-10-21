e13e19d2840845daf37b5cce205614d4aedb63df
Jose A. Alvarado <megaalpha200@gmail.com>
2018-10-21 14:30:58 -0500

    *Added ability to subtract, multiply, and divide, as well as the negation operation.
    *Added error checking for the case when the denominator is 0 in the
    division operation.
    
    *The program now runs forever until the user enters "quit".

--- cal.py
+++ cal.py
@@ -1,33 +1,71 @@
+NEGATION_SIGN = 'n'
+
 def solveEquation(equ):
     equStack = []
 
     result = 0
 
-    count = 0
-    for i in equ:
-        if i == '+':
-            firstOp = equ[0:count]
-            secondOp = equ[count+1:]
-            equStack.append(int(secondOp))
-            equStack.append(int(firstOp))
-            equStack.append('+')
-            result += calculate(equStack)
-            equStack.clear()
-        count += 1
+    if equ[0] == '-':
+        operand = equ[1:]
+        equStack.append(int(operand))
+        equStack.append(NEGATION_SIGN)
+    else:
+        count = 0
+        for i in equ:
+            if isOperator(i):
+                firstOp = equ[0:count]
+                secondOp = equ[count+1:]
 
+                equStack.append(int(secondOp))
+                equStack.append(int(firstOp))
+                equStack.append(i)
+                
+            count += 1
+
+
+    result = calculate(equStack)
+    equStack.clear()
     return result
+
+def isOperator(op):
+    if op == '+' or op == '-' or op == '*' or op == '/':
+        return True
+    else:
+        return False
 
 def calculate(stack):
     tempStack = stack
 
     temp = tempStack.pop()
-    if temp == '+':
+    
+    if (temp == NEGATION_SIGN):
+        operand = tempStack.pop()
+        return (operand * -1)
+    else:
         firstOp = tempStack.pop()
         secondOp = tempStack.pop()
-        return (firstOp + secondOp)
+
+        if temp == '+':
+            return (firstOp + secondOp)
+        elif temp == '-':
+            return (firstOp - secondOp)
+        elif temp == '*':
+            return (firstOp * secondOp)
+        elif temp == '/':
+            if (secondOp == 0):
+                return 'undefined'
+            else:
+                return (firstOp / secondOp)
 
 def main():
-    equ = input("Please enter an equation and I will provide an answer:\n")    
-    print(solveEquation(equ))
+    while(True):
+        equ = input("Please enter an equation and I will provide an answer (or enter 'quit' to exit):\n")    
+        
+        if(equ == 'quit'):
+            print("\n\nQuitting...")
+            exit(0)
+        else:
+            print(solveEquation(equ))
+
     
 main()
